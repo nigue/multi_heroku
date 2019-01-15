315fbb0a5cd647ea822927e8ad11093769070cec
mfuenzalida <mfuenzalida@modyo.com>
2019-01-15 11:38:20 -0300

    avance

---
+++ process/src/main/java/com/tapir/org/controller/TestingController.java
@@ -0,0 +1,27 @@
+package com.tapir.org.controller;
+
+import com.tapir.org.BasicResponse;
+import com.tapir.org.BasicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tapor")
+public class TestingController {
+
+    @Value( "${test.saludo}" )
+    private String saludo;
+
+    @Autowired
+    private BasicService basicService;
+
+    @RequestMapping("/api/helloworld")
+    public ResponseEntity<BasicResponse> findAll(){
+        BasicResponse result = basicService.getBasic(saludo);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
